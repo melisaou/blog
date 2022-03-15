edc36e133da148aa367ebde0c31bb12b74d4c16f
melisaou <melisaangelina08@gmail.com>
2022-03-15 22:50:54 +0700

    Remove whitenoise from installed apps to middleware settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -44,7 +44,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # add this line
     'user',
     'blog',
 ]
@@ -57,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
